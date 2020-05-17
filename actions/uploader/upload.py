9b771417476a3fe194cfed20784b5e0585601e58
Mitchel Humpherys <mitch@directangular.com>
2020-05-17 11:25:21 -0600

    uploader: Workaround for chromedriver error
    https://stackoverflow.com/a/50642913/209050

--- actions/uploader/upload.py
+++ actions/uploader/upload.py
@@ -12,6 +12,8 @@ class MyDriver(webdriver.Chrome):
     def __init__(self, *args, **kwargs):
         options = Options()
         options.add_argument('headless')
+        options.add_argument("no-sandbox")
+        options.add_argument("disable-dev-shm-usage")
         options.add_experimental_option('detach', True)
         super().__init__(options=options)
 
