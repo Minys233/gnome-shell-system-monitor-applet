2de47344aed94025d47a6fb9e202b24b315ecbf8
Mitchel Humpherys <mitch@directangular.com>
2020-05-17 14:29:09 -0600

    actions: syncer: Configure git http post buffer
    https://stackoverflow.com/a/46171246/209050
    
    Fixes: https://github.com/mgalgs/gnome-shell-system-monitor-applet/runs/683351054

--- actions/syncer/run.sh
+++ actions/syncer/run.sh
@@ -7,6 +7,8 @@ ssh-keyscan github.com >> ~/.ssh/known_hosts
 
 git config --global user.email mitch.special@gmail.com
 git config --global user.name "Mitchel Humpherys"
+git config --global http.postBuffer 1048576000
+
 git remote add upstream https://github.com/paradoxxxzero/gnome-shell-system-monitor-applet.git
 git fetch upstream
 git rebase upstream/master || {
