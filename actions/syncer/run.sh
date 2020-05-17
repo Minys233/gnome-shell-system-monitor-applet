bc1d1dcdcd30600277a8a993206da79da8f26049
Mitchel Humpherys <mitch@directangular.com>
2020-05-17 14:13:18 -0600

    actions: syncer: Use https for upstream fetching

--- actions/syncer/run.sh
+++ actions/syncer/run.sh
@@ -5,7 +5,7 @@ set -e
 mkdir -pv ~/.ssh
 ssh-keyscan github.com >> ~/.ssh/known_hosts
 
-git remote add upstream git@github.com:paradoxxxzero/gnome-shell-system-monitor-applet.git
+git remote add upstream https://github.com/paradoxxxzero/gnome-shell-system-monitor-applet.git
 git fetch upstream
 git rebase upstream/master || {
     echo "Couldn't rebase against upstream :(. Please rebase manually."
